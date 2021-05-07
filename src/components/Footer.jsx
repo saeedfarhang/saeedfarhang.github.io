8242121d702a96e17ae8d72f1abd9e2e7587bb02
saeed farhang <saeedfarhang.w@gmail.com>
2021-05-07 12:12:48 +0430

    add resume file

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
 import styled from "styled-components";
 import Button from "../elements/Button";
